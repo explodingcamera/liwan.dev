5158c1c73798309384b072c6f0cdc60587aa3933
Henry Gressmann <mail@henrygressmann.de>
2024-09-16 17:49:51 +0200

    chore: update css
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,69 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import Caddyfile from "./src/assets/caddyfile.tmLanguage.json";
 
+const starlightConfig = starlight({
+	title: "Liwan",
+	favicon: "/favicon.svg",
+	head: [
+		{
+			tag: "link",
+			attrs: {
+				rel: "icon",
+				href: "/favicon.svg",
+				type: "image/svg+xml",
+			},
+		},
+		{
+			tag: "link",
+			attrs: {
+				rel: "icon",
+				href: "/favicon.ico",
+				sizes: "any",
+			},
+		},
+	],
+	expressiveCode: {
+		themes: ["dracula"],
+	},
+	logo: {
+		src: "./src/assets/favicon.svg",
+	},
+	customCss: ["@fontsource-variable/figtree", "./src/styles/custom.css"],
+	description: "Liwan is a lightweight web analytics tool that respects your users' privacy.",
+	credits: false,
+	social: {
+		github: "https://github.com/explodingcamera/liwan",
+	},
+	components: {
+		Footer: "./src/components/Footer.astro",
+		Head: "./src/components/Head.astro",
+	},
+	sidebar: [
+		{
+			label: "Start Here",
+			items: [
+				{ label: "Getting Started", slug: "getting-started" },
+				{
+					label: "Adding your first Website",
+					slug: "adding-your-first-website",
+				},
+			],
+		},
+		{
+			label: "Guides",
+			autogenerate: { directory: "guides" },
+		},
+		{
+			label: "Reference",
+			autogenerate: { directory: "reference" },
+		},
+		{
+			label: "Client Libraries",
+			items: [{ label: "JavaScript/Typescript", link: "https://www.npmjs.com/package/liwan-tracker" }],
+		},
+	],
+});
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://liwan.dev",
@@ -11,65 +74,5 @@ export default defineConfig({
 			langs: [Caddyfile as any],
 		},
 	},
-	integrations: [
-		starlight({
-			title: "Liwan",
-			favicon: "/favicon.svg",
-			head: [
-				{
-					tag: "link",
-					attrs: {
-						rel: "icon",
-						href: "/favicon.svg",
-						type: "image/svg+xml",
-					},
-				},
-				{
-					tag: "link",
-					attrs: {
-						rel: "icon",
-						href: "/favicon.ico",
-						sizes: "any",
-					},
-				},
-			],
-			logo: {
-				src: "./src/assets/favicon.svg",
-			},
-			customCss: ["@fontsource-variable/figtree", "./src/styles/custom.css"],
-			description: "Liwan is a lightweight web analytics tool that respects your users' privacy.",
-			credits: false,
-			social: {
-				github: "https://github.com/explodingcamera/liwan",
-			},
-			components: {
-				Footer: "./src/components/Footer.astro",
-				Head: "./src/components/Head.astro",
-			},
-			sidebar: [
-				{
-					label: "Start Here",
-					items: [
-						{ label: "Getting Started", slug: "getting-started" },
-						{
-							label: "Adding your first Website",
-							slug: "adding-your-first-website",
-						},
-					],
-				},
-				{
-					label: "Guides",
-					autogenerate: { directory: "guides" },
-				},
-				{
-					label: "Reference",
-					autogenerate: { directory: "reference" },
-				},
-				{
-					label: "Client Libraries",
-					items: [{ label: "JavaScript/Typescript", link: "https://www.npmjs.com/package/liwan-tracker" }],
-				},
-			],
-		}),
-	],
+	integrations: [starlightConfig],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,9 @@ export const collections = {
 	docs: defineCollection({
 		schema: docsSchema({
 			extend: z.object({
-				banner: z.object({ content: z.string() }).default({
-					content: `Liwan is currently in beta. If you encounter any issues or have feedback, please let us know on <a href="https://github.com/explodingcamera/liwan/issues">Github</a>.`,
-				}),
+				// banner: z.object({ content: z.string() }).default({
+				// 	content: `Liwan is currently in beta. If you encounter any issues or have feedback, please let us know on <a href="https://github.com/explodingcamera/liwan/issues">Github</a>.`,
+				// }),
 			}),
 		}),
 	}),
