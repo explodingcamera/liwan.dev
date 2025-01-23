8610bc8189679969c53da8b4787e9e1c17feff62
Henry Gressmann <mail@henrygressmann.de>
2025-01-23 15:24:50 +0100

    chore: update deps
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 import Caddyfile from "./src/assets/caddyfile.tmLanguage.json";
 
 const starlightConfig = starlight({
