38927efb674cf052e1bf50f12ea36c106454023f
Henry <mail@henrygressmann.de>
2026-03-18 18:29:47 +0100

    chore: add comparision
    Signed-off-by: Henry <mail@henrygressmann.de>

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Licensed under either of [Apache License, Version 2.0](./LICENSE-APACHE) or [MIT license](./LICENSE-MIT) at your option.
 
-Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in TinyWasm by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in liwan by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
--- astro.config.ts
+++ astro.config.ts
@@ -5,97 +5,105 @@ import Caddyfile from "./src/assets/caddyfile.tmLanguage.json";
 import react from "@astrojs/react";
 
 const starlightConfig = starlight({
-    title: "Liwan",
-    favicon: "/favicon.svg",
-    head: [
-        {
-            tag: "link",
-            attrs: {
-                rel: "icon",
-                href: "/favicon.svg",
-                type: "image/svg+xml",
-            },
-        },
-        {
-            tag: "link",
-            attrs: {
-                rel: "icon",
-                href: "/favicon.ico",
-                sizes: "any",
-            },
-        },
-    ],
-    expressiveCode: {
-        themes: ["dracula"],
-    },
-    logo: {
-        src: "./src/assets/favicon.svg",
-    },
-    customCss: [
-        "@fontsource-variable/stack-sans-headline",
-        "@fontsource-variable/stack-sans-text",
-        "./src/styles/custom.css",
-    ],
-    description: "Liwan is a lightweight web analytics tool that respects your users' privacy.",
-    credits: false,
-    social: [
-        {
-            icon: "github",
-            label: "GitHub",
-            href: "https://github.com/explodingcamera/liwan",
-        },
-    ],
-    components: {
-        Footer: "./src/components/Footer.astro",
-        Head: "./src/components/Head.astro",
-    },
-    sidebar: [
-        {
-            label: "Getting Started",
-            slug: "getting-started",
-        },
-        {
-            label: "Installation",
-            items: [
-                {
-                    label: "Single Binary",
-                    slug: "install/binary",
-                },
-                {
-                    label: "Docker",
-                    slug: "install/docker",
-                },
-                {
-                    label: "Docker Compose",
-                    slug: "install/docker-compose",
-                },
-            ],
-        },
-        {
-            label: "Guides",
-            autogenerate: { directory: "guides" },
-        },
-        {
-            label: "Reference",
-            autogenerate: { directory: "reference" },
-        },
-        {
-            label: "Client Libraries",
-            items: [
-                { label: "JavaScript/Typescript", link: "https://www.npmjs.com/package/liwan-tracker" },
-            ],
-        },
-    ],
+	title: "Liwan",
+	favicon: "/favicon.svg",
+	head: [
+		{
+			tag: "link",
+			attrs: {
+				rel: "icon",
+				href: "/favicon.svg",
+				type: "image/svg+xml",
+			},
+		},
+		{
+			tag: "link",
+			attrs: {
+				rel: "icon",
+				href: "/favicon.ico",
+				sizes: "any",
+			},
+		},
+	],
+	expressiveCode: {
+		themes: ["dracula"],
+	},
+	logo: {
+		src: "./src/assets/favicon.svg",
+	},
+	customCss: [
+		"@fontsource-variable/stack-sans-headline",
+		"@fontsource-variable/stack-sans-text",
+		"./src/styles/custom.css",
+	],
+	description: "Liwan is a lightweight web analytics tool that respects your users' privacy.",
+	credits: false,
+	social: [
+		{
+			icon: "github",
+			label: "GitHub",
+			href: "https://github.com/explodingcamera/liwan",
+		},
+	],
+	components: {
+		Footer: "./src/components/Footer.astro",
+		Head: "./src/components/Head.astro",
+	},
+	sidebar: [
+		{
+			label: "Getting Started",
+			slug: "getting-started",
+		},
+		{
+			label: "Installation",
+			items: [
+				{
+					label: "Single Binary",
+					slug: "install/binary",
+				},
+				{
+					label: "Docker",
+					slug: "install/docker",
+				},
+				{
+					label: "Docker Compose",
+					slug: "install/docker-compose",
+				},
+			],
+		},
+		{
+			label: "Guides",
+			autogenerate: { directory: "guides" },
+		},
+		{
+			label: "Reference",
+			autogenerate: { directory: "reference" },
+		},
+		{
+			label: "Client Libraries",
+			items: [
+				{ label: "JavaScript/Typescript", link: "https://www.npmjs.com/package/liwan-tracker" },
+			],
+		},
+		{
+			label: "Collected Data",
+			slug: "collected-data",
+		},
+		{
+			label: "Comparison",
+			slug: "comparison",
+		},
+	],
 });
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://liwan.dev",
-    markdown: {
-        shikiConfig: {
-            // biome-ignore lint/suspicious/noExplicitAny: Type is correct
-            langs: [Caddyfile as any],
-        },
-    },
-    integrations: [starlightConfig, react()],
+	site: "https://liwan.dev",
+	markdown: {
+		shikiConfig: {
+			// biome-ignore lint/suspicious/noExplicitAny: Type is correct
+			langs: [Caddyfile as any],
+		},
+	},
+	integrations: [starlightConfig, react()],
 });
